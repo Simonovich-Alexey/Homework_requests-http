a49c57f90b11280a2bbb2c40585ca5efdfcb6ae8
Alexey_Sim <simonovich_alexey@bk.ru>
2023-07-12 23:17:43 +0500

    Changes main

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
+import requests
 
+
+def get_token():
+    with open("sourse/token.txt") as file:
+        token = file.read()
+        return token
+
+
+url = 'https://cloud-api.yandex.net/v1/disk/resources'
+params = {"path": "/"}
+headers = {"Authorization": "OAuth " + get_token()}
+
+response = requests.get(url, headers=headers, params=params)
 
 if __name__ == '__main__':
-    pass
+    print(response.status_code)
+    if 200 <= response.status_code <= 300:
+        print(f'Content : {response.content}')
+        print(f'Text : {response.text}')
+        data = response.json()
+        print(f'JSON : {data}')
+
